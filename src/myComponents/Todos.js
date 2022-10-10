bea2f904791afe46b3d8e6afc37bb2accc9f6e59
musta55 <bsse1114@iit.du.ac.bd>
2022-10-11 03:47:46 +0600

    fixed empty render

--- src/myComponents/Todos.js
+++ src/myComponents/Todos.js
@@ -6,10 +6,9 @@ const Todos = (props) => {
     <div className="container">
     
      <h3 className="text-center my-3">Todo list</h3>
-     {
-      props.todos.map((todo)=>{
+     {props.todos.length===0?"No todos to display":
+     props.todos.map((todo)=>{
         return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-
       })
      }
      
